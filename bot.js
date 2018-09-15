6fe1898796a8aadd9402276eba5c31d07b5a2b63
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-09-15 13:01:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -292,7 +292,7 @@ function play(guild, song) {
 	const serverQueue = queue.get(guild.id);
 
 		if (!song) {
-		serverQueue.voiceChannel.join();
+		serverQueue.voiceChannel.leave();
 		queue.delete(guild.id);
 		return;
 	}
