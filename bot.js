16bb9d7b26a2987dd483eb5c5cf83aae13aa7e66
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-09-16 19:24:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -347,19 +347,7 @@ client.on('message', message => {
 
 });
   
-  if (message.content === '1play') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { 
-          message.reply('لقد دخلت الروم بنجاح !');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('يجب ان تكون في روم صوتي');
-    }
-  }
-});
-
+ 
 
 client.on('ready', () => {
     client.channels.find(c => c.id === '490245960195112970').join();
